75a18bcf26690354017a2d80e5d0878a0fe3ad86
ag113ate <shota.terui@beach.ocn.ne.jp>
2019-01-19 14:18:10 +0000

    Add FizzBuzz program

--- sample.rb
+++ sample.rb
@@ -7,3 +7,15 @@ fruits = ["banana", "apple", "lemon"]
 fruits.each do |fruit|
   puts fruit
 end
+
+for i in 1..20
+  if ((i %15) == 0)
+    puts "FizzBuzz"
+  elsif ((i % 3) == 0)
+    puts "Fizz"
+  elsif ((i % 5) == 5)
+    puts "Buzz"
+  else
+    puts i
+  end
+end
